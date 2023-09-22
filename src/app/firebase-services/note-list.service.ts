2226ecbca0bf4a3f3e3ab7ad76e350e697e789bf
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-09-22 20:56:08 +0200

    firebase

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { inject } from '@angular/core';
-import { Firestore, collection } from '@angular/fire/firestore';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -14,11 +14,15 @@ export class NoteListService {
 
   //const itemCollection = collection(this.firestore, 'items');
 
-  getNoteRef(){
+  getNotesRef(){
     return collection(this.firestore, 'notes');
   }
 
   getTrashRef(){
     return collection(this.firestore, 'trash');
+  }
+
+  getsingelDocRef(colId:string, docId:string){
+    return doc(collection(this.firestore, colId) ,docId)
   }
 }
